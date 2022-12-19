1e34ae96b68f18c072e2886764457a93e3208a4d
Main.kt <muhammad.saheed.iam@gmail.com>
2022-12-19 22:21:56 +0530

    Add explanation for day-01

--- day-01/solution/main.cpp
+++ day-01/solution/main.cpp
@@ -13,5 +13,12 @@ auto main() -> int {
        else if (i % 2 == 0) std::cout << "Bar\n";
        else std::cout << "Baz\n";
     }
-    
+
+    std::cout << "Explanation\n";
+    for (auto i{a}; i <= b; i++) {
+       if (i % 3 == 0) std::cout << "* " << i << R"( is multiple of three, hence "Foo")" << '\n';
+       else if (i % 2 == 0) std::cout << "* " << i << R"( is even, hence "Bar")" << '\n';
+       else std::cout << "* " << i << R"( is odd, hence "Baz")" << '\n';
+    }
+
 }
