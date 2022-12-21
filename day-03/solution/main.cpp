665674cfa45db067536fb613a23eabe9597d2cd6
Main.kt <muhammad.saheed.iam@gmail.com>
2022-12-22 01:36:18 +0530

    Refactor main.cpp

--- day-03/solution/main.cpp
+++ day-03/solution/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <ranges>
 #include <numeric>
 
 auto main() -> int {
@@ -8,22 +8,17 @@ auto main() -> int {
     int n{};
     std::cin >> n;
 
-    std::vector<int> numbers(n);
+    std::vector<double> numbers(n);
     for (auto &number : numbers) std::cin >> number;
 
     auto const average{
-        std::reduce(numbers.begin(), numbers.end()) / static_cast<int>(numbers.size())
+        std::reduce(numbers.begin(), numbers.end()) / n
     };
 
-    std::vector<int> filtered;
-    std::copy_if(
-        numbers.begin(), 
-        numbers.end(),
-        std::back_inserter(filtered),
-        [&](int i) { return i > average; }
-    );
-
-    for (auto const number : filtered) std::cout << number << ' ';
+    for (auto const number : std::views::all(numbers)
+                           | std::views::filter([&](double i) { return i > average; })) {
+        std::cout << number << ' ';
+    }
     std::cout << '\n';
 
 }
