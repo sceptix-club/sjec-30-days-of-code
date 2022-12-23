6028b2a8f2d8b14a6f2b0bec4d0a2bf75fce314e
devprabhu18 <107057399+devprabhu18@users.noreply.github.com>
2022-12-23 19:27:23 +0530

    Update day4dev.cpp

--- day-04/day4dev.cpp
+++ day-04/day4dev.cpp
@@ -7,9 +7,9 @@ int main(){
     cin>>m;
     cout<<"Enter the number of rows"<<endl;
     cin>>n;
-    if(m%3==0 && n%2==0)
+    if(m%3==0 && n%2==0)                  //checking wether the colums are multiple of 3 and rows are even in number
         cout<<"Yes"<<endl;
-    else if(m%2==0 && n%3==0)
+    else if(m%2==0 && n%3==0)             //checking wether the rows are multiple of 3 and colums are even in number
         cout<<"Yes"<<endl;
     else
         cout<<"No"<<endl;                                                                                                                                                                                                                          
