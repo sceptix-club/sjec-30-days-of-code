5a819cbaf38df47e633a7c8f1180bd0875ab8725
ABHINAV C YADAV <85658995+abhi-abhinav@users.noreply.github.com>
2022-12-23 23:15:27 +0530

    Update day04.c

--- day-04/day04.c
+++ day-04/day04.c
@@ -3,9 +3,9 @@
 int main(void)
 {
     int m, n;
-    printf("Enter number of colums: ");
-    scanf("%d", &m);
     printf("Enter number of rows: ");
+    scanf("%d", &m);
+    printf("Enter number of columns: ");
     scanf("%d", &n);
 
     if ((m % 2 == 0 && n % 3 == 0) || (m % 3 == 0 && n % 2 == 0))
