b5b064a4066c38112a1fc00a4802145537cc2083
pranavvraja <pranavvraja@gmail.com>
2023-01-01 17:13:59 +0530

    updated day-09

--- day-09/diamond-intuition.cpp
+++ day-09/diamond-intuition.cpp
@@ -1,4 +1,5 @@
-#include <iostream.h>
+#include <iostream>
+using namespace std;
 
 int main()
 {
@@ -6,27 +7,30 @@ int main()
     
     cout<<"Enter width:";
     cin>>n;
-    m=n-1;
-    for(i=0;i<n;i++)
-        {   
-            for(j=0;j<=m;j++)
-                cout<<" ";
-        
-            for(j=0;j<=i;j++)
-                cout<<"# ";
-            cout<<endl;
-            m--;
-        }
-    m=2;
-    for(i=n-1;i>0;i--)
-        {   
-            for(j=0;j<m;j++)
-                cout<<" ";
+    if(n>0 && n<=200)
+    {
+        m=n-1;
+        for(i=0;i<n;i++)
+            {   
+                for(j=0;j<=m;j++)
+                    cout<<" ";
             
-            for(j=0;j<i;j++)
-                cout<<"# ";
-            cout<<endl;
-            m++;
-        }
+                for(j=0;j<=i;j++)
+                    cout<<"# ";
+                cout<<endl;
+                m--;
+            }
+        m=2;
+        for(i=n-1;i>0;i--)
+            {   
+                for(j=0;j<m;j++)
+                    cout<<" ";
+                
+                for(j=0;j<i;j++)
+                    cout<<"# ";
+                cout<<endl;
+                m++;
+            }
+    }
     return 0;
 }
