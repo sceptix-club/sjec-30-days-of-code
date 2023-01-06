ee110b0194f9151e17caed259751943407f7b062
JOSHUA5227 <joshua.quinthino@gmail.com>
2023-01-06 22:41:49 +0530

    day 18 done?

--- day-18/day18sol.c
+++ day-18/day18sol.c
@@ -2,13 +2,14 @@
 #include<string.h>
 #include<ctype.h>
 
-const char letters[26]={
+ char letters[26]={
     'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z' // 0- 25 array index
 };
 int main()
 {   
-    int i,j,k,arr[26];
-    char sen[1000];
+    int i,j,k,temp;
+    float arr[26];
+    char sen[1000],temp1;
     printf("Enter your sentence : \n");
     scanf("%s",sen);
     i=strlen(sen);
@@ -25,12 +26,22 @@ int main()
             }
         }
     }
-    for(j=0;j<5;j++)
-    {
-        for(k=0;k<26;k++)
+    for(k=0;k<26;k++)
+    arr[k]=(arr[k]/i)*100;
+    
+    for(k=0;k<26;k++)
+    for(j=k+1;j<26;j++)
+        if(arr[k]<arr[j])
         {
-            if(arr[k]>=arr[k+1])
-            
+            temp=arr[k];
+            arr[k]=arr[j];
+            arr[j]=temp;
+            temp1=letters[k];
+            letters[k]=letters[j];
+            letters[j]=temp1;
         }
+    for(k=0;k<5;k++)
+    {
+        printf("%c = %.2f \n",letters[k],arr[k]);
     }
 }
