f231cca070043f9e895c16874c3039f3a7bf26f8
abhi-abhinav <acyadav805@gmail.com>
2023-01-25 21:25:21 +0530

    changes

--- challenge-submissons/abhi-abhinav/day-22/golfed_day22.c
+++
@@ -1,3 +0,0 @@
-#include<stdio.h>
-int f(int n,int s){return n==1?s:f(n%2?3*n+1:n/2,s+1);} int main(){int a,b,i,m,mi;scanf("%d%d",&a,&b);if(a>0&&a<=b&&b<=10000)for(i=a;i<=b;i++)if(f(i,0)>m)m=f(i,0),mi=i;printf("%d",mi);}
-
